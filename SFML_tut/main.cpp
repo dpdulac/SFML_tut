7f31a6c12c60bb48866049de0f03858b299147fb
daviddulac <dpdulac@hotmail.com>
2021-04-27 22:23:23 +0200

    Initial Commit

---
+++ SFML_tut/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SFML_tut
+//
+//  Created by David Dulac on 27/04/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
